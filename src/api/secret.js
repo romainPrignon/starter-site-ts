9eee00c775c7ecc208c409e7cd6006d4967b0b8f
romainprignon <pro.rprignon@gmail.com>
2017-11-25 10:38:57 +0100

    int: fix backend url

--- src/api/secret.js
+++ src/api/secret.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export const getSecretById = async (secretId) => {
-  const response = await axios.get(`http://localhost:4000/whisper/${secretId}`)
+  const response = await axios.get(`https://sketch-api-tnlkvccvym.now.sh/whisper/${secretId}`)
 
   return response.data
 }
 
 export const postSecret = async (secret) => {
-  const response = await axios.post(`http://localhost:4000/listen`, secret)
+  const response = await axios.post(`https://sketch-api-tnlkvccvym.now.sh/listen`, secret)
 
   return response.data
 }
