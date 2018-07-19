57b768aaf5f8504241b778098edc54f289fc2c6e
romainprignon <pro.rprignon@gmail.com>
2018-07-19 21:11:38 +0200

    feat: allow cors, use alias for backend

--- src/api/secret.js
+++ src/api/secret.js
@@ -1,13 +1,20 @@
 import axios from 'axios'
 
 export const getSecretById = async (secretId) => {
-  const response = await axios.get(`https://sketch-api-tnlkvccvym.now.sh/whisper/${secretId}`)
+  const response = await axios.get(
+    `https://sketch-api.romainprignon.fr/whisper/${secretId}`,
+    { headers: { 'Access-Control-Allow-Origin': '*' } }
+  )
 
   return response.data
 }
 
 export const postSecret = async (secret) => {
-  const response = await axios.post(`https://sketch-api-tnlkvccvym.now.sh/listen`, secret)
+  const response = await axios.post(
+    `https://sketch-api.romainprignon.fr/listen`,
+    secret,
+    { headers: { 'Access-Control-Allow-Origin': '*' } }
+  )
 
   return response.data
 }
